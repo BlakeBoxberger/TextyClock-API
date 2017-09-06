c2f6e62b395a36be98eaaa4b94524f10b0fed14e
NeinZedd9 <blake.boxberger@gmail.com>
2017-09-06 13:03:00 -0700

    Update NZ9_TextyClockFormatter.h

--- NZ9_TextyClockFormatter.h
+++ NZ9_TextyClockFormatter.h
@@ -2,13 +2,4 @@
 
 + (NSString *)getTimeString;            // This will retrieve the correct locale for the time string
 
-+ (NSString *)getEnglishTimeString;     // These will return the translations for a specific locale.
-
-+ (NSString *)getFrenchTimeString;
-
-+ (NSString *)getBelgianTimeString;
-
-+ (NSString *)getSwissTimeString;
-// Add your language methods here.
-
 @end
